2aa5471ada5081c8368cd5ce91e2a21a1b78862d
Dominic <dominicmph@gmail.com>
2020-12-18 14:37:06 -0800

    formatting

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,30 @@ public class Main {
 
 		/* B
 		* Write a statement that will call the display method to display the light sequence for the gradShow object.
-        * Write the statement below.
+		* Write the statement below.
 		* */
 		gradShow.display();
 		// Basic function call
 
 		/* C
 		* Write a statement that will be used to update the gradShow light sequence to "0011 0011 0011".
-        * Write the statement below.
+		* Write the statement below.
 		* */
 		gradShow.changeSequence("0011 0011 0011");
 		// Basic function call w/ parameter as provided
 
 		/* D
 		* Write a code segment that will call the insertSegment method to insert the segment "1111 1111" in the current sequence for gradShow at index 4. The resulting sequence will be stored in the string resultSeq.
-        * Write the code segment below.
+		* Write the code segment below.
 		* */
 		String resultSeq = gradShow.insertSegment("1111 1111", 4);
 		// Basic function call w/ parameters as provided
 
 		/* E
 		* Assume that the string oldSeq has been properly declared and initialized and contains the string segment. Write a code segment that will remove the first occurrence of segment from oldSeq and store it in the string newSeq. Consider the following examples.
-        * If oldSeq is "1100000111" and segment is "11", then "00000111" should be stored in newSeq.
-        * If oldSeq is "0000011" and segment is "11", then "00000" should be stored in newSeq.
-        * If oldSeq is "1100000111" and segment is "00", then "11000111" should be stored in newSeq.
+		* If oldSeq is "1100000111" and segment is "11", then "00000111" should be stored in newSeq.
+		* If oldSeq is "0000011" and segment is "11", then "00000" should be stored in newSeq.
+		* If oldSeq is "1100000111" and segment is "00", then "11000111" should be stored in newSeq.
 		* */
 		// Sample data to test
 		String oldSeq = "1100000111";
@@ -46,8 +46,8 @@ public class Main {
 
 		/* F
 		* Two lights will be arranged on a two-dimensional plane. The vertical distance between the two lights is stored in the double variable a. The horizontal distance between the two lights is stored in the double variable b.
-        * The straight-line distance between the two lights is given by the formula square root of the quantity a squared plus b squared.
-        * Write a code segment that prints the straight-line distance between the two lights according to the formula above.
+		* The straight-line distance between the two lights is given by the formula square root of the quantity a squared plus b squared.
+		* Write a code segment that prints the straight-line distance between the two lights according to the formula above.
 		* */
 		// Sample data to test
 		double a = 3;
